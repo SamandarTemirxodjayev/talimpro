55c50ed024b564593868c717ca5f21d8f9cb04cb
Samandar <iwtuz166@gmail.com>
2024-07-24 17:21:14 +0500

    add server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-consoele.log("Hello world!");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 3005, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
+});
