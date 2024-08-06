1bf8e56b82ab972ee459c62ee598ac9dc600ae10
Samandar <iwtuz166@gmail.com>
2024-08-06 11:13:04 +0500

    modify cors

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ require("dotenv").config();
 const app = express();
 const routes = require("./routes/router.js");
 
-const corsOptions = {
-	origin: "*",
-};
-
-app.use(cors(corsOptions));
-
 app.use(express.json());
 
 mongoose
 	.connect(process.env.MONGODB_URI)
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Could not connect to MongoDB:", err));
+	
+app.use(cors());
 
 app.listen(process.env.PORT || 3005, () => {
 	console.log(`Server listening on port ${process.env.PORT || 3005}`);
