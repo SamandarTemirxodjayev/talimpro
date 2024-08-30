69272cdd5f83f267992b6600cf7b0b72c1478185
Samandar <iwtuz166@gmail.com>
2024-08-30 12:30:04 +0500

    update delete class

--- controllers/schools.controller.js
+++ controllers/schools.controller.js
@@ -283,7 +283,7 @@ exports.deleteClass = async (req, res) => {
 	try {
 		await Classes.findByIdAndDelete(req.params.id);
 		await Pupils.deleteMany({
-			class: req.params.id,
+			class: parseInt(req.params.id),
 		});
 		return res.status(200).json({
 			status: "success",
