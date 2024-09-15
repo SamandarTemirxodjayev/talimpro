270112043e1b05f08efd2a293920b2f80c8de680
Samandar <iwtuz166@gmail.com>
2024-09-15 22:13:08 +0500

    modify pupils

--- models/Pupils.js
+++ models/Pupils.js
@@ -1,45 +1,55 @@
 const {Schema, model} = require("mongoose");
-const { AutoIncrement } = require("../utils/helpers");
+const {AutoIncrement} = require("../utils/helpers");
 
-const pupilsSchema = new Schema({
-	_id: {
-		type: Number,
+const pupilsSchema = new Schema(
+	{
+		_id: {
+			type: Number,
+		},
+		school: {
+			type: Number,
+			ref: "schools",
+		},
+		class: {
+			type: Number,
+			ref: "classes",
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		surname: {
+			type: String,
+			required: true,
+		},
+		father_name: {
+			type: String,
+			required: true,
+		},
+		phone_number: {
+			type: String,
+		},
+		login: {
+			type: String,
+		},
+		password: {
+			type: String,
+		},
+		createdAt: {
+			type: Number,
+			default: Date.now(),
+		},
 	},
-	school: {
-		type: Number,
-		ref: "schools",
+	{
+		versionKey: false,
 	},
-	class: {
-		type: Number,
-		ref: "classes",
-	},
-	name: {
-		type: String,
-		required: true,
-	},
-	surname: {
-		type: String,
-		required: true,
-	},
-	father_name: {
-		type: String,
-		required: true,
-	},
-	login: {
-		type: String,
-	},
-	password: {
-		type: String,
-	},
-	createdAt: {
-		type: Number,
-		default: Date.now()
-	}
-}, {
-	versionKey: false
+);
+
+pupilsSchema.plugin(AutoIncrement, {
+	modelName: "pupil",
+	fieldName: "_id",
+	startAt: 1000,
 });
-
-pupilsSchema.plugin(AutoIncrement, { modelName: 'pupil', fieldName: '_id', startAt: 1000 })
 const Pupils = model("pupils", pupilsSchema);
 
 module.exports = Pupils;
